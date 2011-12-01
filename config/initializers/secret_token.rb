9864c8ad51241c2627c80d1a211fb2c51a246bd8
John Zhang <theparasite@gmail.com>
2011-12-01 00:46:48 -0600

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bearsforsmiles::Application.config.secret_token = '2b6ade618e7bf818059687d58876e617190dbcee05b1e08904292ba8926de55779690c6b57da5ddad27e491f50176a18d0b052ae8c0838111c334c30ff4a25ff'
